9de1bbcb4cb2aeed433b8160aec8b5d3872eb1c3
cuong-ich-truong <cuongichtruong@gmail.com>
2024-07-24 17:11:49 +0700

    updates

--- src/index.ts
+++ src/index.ts
@@ -16,3 +16,5 @@ export * from './events/base-publisher';
 export * from './events/base-ticket-event';
 export * from './events/ticket-created-event';
 export * from './events/ticket-updated-event';
+
+export * from './events/types/order-status';
